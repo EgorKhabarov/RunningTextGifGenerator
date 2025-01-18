9ca5c6af5fa7391c3261879b9cb1f2d6ddf92eac
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-01-19 03:49:54 +0400

    update

--- tests/test_GIF.py
+++ tests/test_GIF.py
@@ -24,7 +24,9 @@ def compare(gif: GIF, num: int):
 
     result = test_content == result_content
     if not result:
-        print(f"{len(test_content)=} {len(result_content)=} {hash(test_content)=} {hash(result_content)=}")
+        print(
+            f"{len(test_content)=} {len(result_content)=} {hash(test_content)=} {hash(result_content)=}"
+        )
     return result
 
 
