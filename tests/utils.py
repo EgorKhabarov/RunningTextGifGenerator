9d8646cc16c60f5f1ec3c479b1c3c089b3cd450d
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-01-20 23:40:26 +0400

    update tests

--- tests/utils.py
+++ tests/utils.py
@@ -35,10 +35,42 @@ def compare_gif(gif: GIF, path: str | Path):
 
     test_frames = GIF.extract_gif_frames(test_gif)
     result_frames = GIF.extract_gif_frames(result_gif)
-    for (test_image, _), (result_image, _) in zip_longest(test_frames, result_frames):
+    for test_image, result_image in zip_longest(test_frames, result_frames):
         if None in (test_image, result_image):
             return False
+
+        (test_image, _), (result_image, _) = test_image, result_image
 
         if test_image != result_image:
             return False
     return True
+
+
+def test_utils():
+    with ExceptionWrapper(Exception(f"Expected exception ValueError('error') was not raised")):
+        with ExceptionWrapper(ValueError("error")):
+            pass
+
+    with ExceptionWrapper(Exception(f"ValueError('error') != ValueError('exception')")):
+        with ExceptionWrapper(ValueError("error")):
+            raise ValueError("exception")
+
+    assert compare_gif(
+        GIF.open(
+            "tests/result_images/test_direction/1/test_direction_1_True_True.gif",
+            progress_bar=False,
+        ),
+        "tests/result_images/test_direction/2/test_direction_2_True_False.gif",
+    ) is False
+
+    gif1 = GIF(progress_bar=False)
+    gif1.add_text_fragment("text", outro=False)
+
+    gif2 = GIF(progress_bar=False)
+    gif2.add_text_fragment("text", outro=True)
+    gif2.save("tests/result_images/test_utils/1/test_utils_1.gif")
+
+    assert compare_gif(
+        gif1,
+        "tests/result_images/test_utils/1/test_utils_1.gif",
+    ) is False
