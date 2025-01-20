b1804a745dc5a0c77542a36d61cf4f747b080046
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-01-20 23:38:48 +0400

    update tests

--- tests/test_direction.py
+++ tests/test_direction.py
@@ -1,310 +1,38 @@
+# noinspection PyPackageRequirements
+import pytest
+
 from gif import GIF
 from tests.utils import compare_gif
 
 
-def test_direction():
+@pytest.mark.parametrize(
+    "num, intro, outro, durations",
+    (
+        (1, True, True, (50, 66, 200, 66, 200, 50, 200, 50, 200, 1000)),
+        (2, True, False, (50, 66, 200, 66, 200, 50, 200, 50, 200, 1000)),
+        (3, False, True, (50, 66, 200, 66, 200, 50, 200, 50, 200, 1000)),
+        (4, False, False, (50, 1000, 1000, 66, 1000, 50, 1000, 50, 1000, 1000)),
+    ),
+)
+def test_direction(num: int, intro: bool, outro: bool, durations: tuple[int]):
     gif = GIF(debug=False, progress_bar=False)
 
-    num = 1
-    intro = True
-    outro = True
     gif.debug_path = (
         f"tests/debug_images/test_direction/{num}/"
         f"test_direction_{num}_{intro}_{outro}_{{fragment_index}}.png"
-    )
-    gif.add_text_fragment(
-        f"--{intro=};{outro=}--",
-        intro=False,
-        outro=False,
-        direction="left",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "left",
-        intro=intro,
-        outro=outro,
-        direction="left",
-        duration=66,
-    )
-    gif.add_text_fragment(
-        "up",
-        intro=intro,
-        outro=outro,
-        direction="up",
-        duration=200,
-    )
-    gif.add_text_fragment(
-        "right",
-        intro=intro,
-        outro=outro,
-        direction="right",
-        duration=66,
-    )
-    gif.add_text_fragment(
-        "down",
-        intro=intro,
-        outro=outro,
-        direction="down",
-        duration=200,
-    )
-    gif.add_text_fragment(
-        "left biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="left",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "up biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="up",
-        duration=200,
-    )
-    gif.add_text_fragment(
-        "right biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="right",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "down biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="down",
-        duration=200,
-    )
-    # gif.save(f"tests/result_images/test_direction/{num}/test_direction_{num}_{intro}_{outro}.gif")
-    assert compare_gif(
-        gif,
-        f"tests/result_images/test_direction/{num}/test_direction_{num}_{intro}_{outro}.gif",
     )
 
-    num = 2
-    intro = True
-    outro = False
-    gif.debug_path = (
-        f"tests/debug_images/test_direction/{num}/"
-        f"test_direction_{num}_{intro}_{outro}_{{fragment_index}}.png"
-    )
-    gif.add_text_fragment(
-        f"--{intro=};{outro=}--",
-        intro=False,
-        outro=False,
-        direction="left",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "left",
-        intro=intro,
-        outro=outro,
-        direction="left",
-        duration=66,
-    )
-    gif.add_text_fragment(
-        "up",
-        intro=intro,
-        outro=outro,
-        direction="up",
-        duration=200,
-    )
-    gif.add_text_fragment(
-        "right",
-        intro=intro,
-        outro=outro,
-        direction="right",
-        duration=66,
-    )
-    gif.add_text_fragment(
-        "down",
-        intro=intro,
-        outro=outro,
-        direction="down",
-        duration=200,
-    )
-    gif.add_text_fragment(
-        "left biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="left",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "up biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="up",
-        duration=200,
-    )
-    gif.add_text_fragment(
-        "right biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="right",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "down biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="down",
-        duration=200,
-    )
-    # gif.save(f"tests/result_images/test_direction/{num}/test_direction_{num}_{intro}_{outro}.gif")
-    assert compare_gif(
-        gif,
-        f"tests/result_images/test_direction/{num}/test_direction_{num}_{intro}_{outro}.gif",
-    )
+    gif.add_text_fragment(f"--{intro=};{outro=}--", intro=False, outro=False, direction="left", duration=durations[0])
+    gif.add_text_fragment("left", intro=intro, outro=outro, direction="left", duration=durations[1])
+    gif.add_text_fragment("up", intro=intro, outro=outro, direction="up", duration=durations[2])
+    gif.add_text_fragment("right", intro=intro, outro=outro, direction="right", duration=durations[3])
+    gif.add_text_fragment("down", intro=intro, outro=outro, direction="down", duration=durations[4])
+    gif.add_text_fragment("left biiiiiig teeexxxt", intro=intro, outro=outro, direction="left", duration=durations[5])
+    gif.add_text_fragment("up biiiiiig teeexxxt", intro=intro, outro=outro, direction="up", duration=durations[6])
+    gif.add_text_fragment("right biiiiiig teeexxxt", intro=intro, outro=outro, direction="right", duration=durations[7])
+    gif.add_text_fragment("down biiiiiig teeexxxt", intro=intro, outro=outro, direction="down", duration=durations[8])
+    gif.add_text_fragment("None", intro=intro, outro=outro, direction="none", duration=durations[9])
 
-    num = 3
-    intro = False
-    outro = True
-    gif.debug_path = (
-        f"tests/debug_images/test_direction/{num}/"
-        f"test_direction_{num}_{intro}_{outro}_{{fragment_index}}.png"
-    )
-    gif.add_text_fragment(
-        f"--{intro=};{outro=}--",
-        intro=False,
-        outro=False,
-        direction="left",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "left",
-        intro=intro,
-        outro=outro,
-        direction="left",
-        duration=66,
-    )
-    gif.add_text_fragment(
-        "up",
-        intro=intro,
-        outro=outro,
-        direction="up",
-        duration=200,
-    )
-    gif.add_text_fragment(
-        "right",
-        intro=intro,
-        outro=outro,
-        direction="right",
-        duration=66,
-    )
-    gif.add_text_fragment(
-        "down",
-        intro=intro,
-        outro=outro,
-        direction="down",
-        duration=200,
-    )
-    gif.add_text_fragment(
-        "left biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="left",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "up biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="up",
-        duration=200,
-    )
-    gif.add_text_fragment(
-        "right biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="right",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "down biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="down",
-        duration=200,
-    )
-    # gif.save(f"tests/result_images/test_direction/{num}/test_direction_{num}_{intro}_{outro}.gif")
-    assert compare_gif(
-        gif,
-        f"tests/result_images/test_direction/{num}/test_direction_{num}_{intro}_{outro}.gif",
-    )
-
-    num = 4
-    intro = False
-    outro = False
-    gif.debug_path = (
-        f"tests/debug_images/test_direction/{num}/"
-        f"test_direction_{num}_{intro}_{outro}_{{fragment_index}}.png"
-    )
-    gif.add_text_fragment(
-        f"--{intro=};{outro=}--",
-        intro=False,
-        outro=False,
-        direction="left",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "left",
-        intro=intro,
-        outro=outro,
-        direction="left",
-        duration=1000,
-    )
-    gif.add_text_fragment(
-        "up",
-        intro=intro,
-        outro=outro,
-        direction="up",
-        duration=1000,
-    )
-    gif.add_text_fragment(
-        "right",
-        intro=intro,
-        outro=outro,
-        direction="right",
-        duration=66,
-    )
-    gif.add_text_fragment(
-        "down",
-        intro=intro,
-        outro=outro,
-        direction="down",
-        duration=1000,
-    )
-    gif.add_text_fragment(
-        "left biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="left",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "up biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="up",
-        duration=1000,
-    )
-    gif.add_text_fragment(
-        "right biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="right",
-        duration=50,
-    )
-    gif.add_text_fragment(
-        "down biiiiiig teeexxxt",
-        intro=intro,
-        outro=outro,
-        direction="down",
-        duration=1000,
-    )
-    # gif.save(f"tests/result_images/test_direction/{num}/test_direction_{num}_{intro}_{outro}.gif")
-    assert compare_gif(
-        gif,
-        f"tests/result_images/test_direction/{num}/test_direction_{num}_{intro}_{outro}.gif",
-    )
+    path = f"tests/result_images/test_direction/{num}/test_direction_{num}_{intro}_{outro}.gif"
+    # gif.save(path)
+    assert compare_gif(gif, path)
