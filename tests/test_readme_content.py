e57431bd9b0f3660420a38d7bb7c29c9c18b6816
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-01-23 17:22:39 +0400

    update

--- tests/test_readme_content.py
+++ tests/test_readme_content.py
@@ -3,6 +3,19 @@ from tests.utils import compare_gif
 
 
 def test_readme_content():
+    gif = GIF(columns=20, rows=20, progress_bar=False)
+    gif.add_image_fragment(
+        image_path="readme_content/frog_jump.png",
+        direction="left",
+        duration=100,
+        speed=21,
+    )
+    gif.color_config["color_pixel_off_light"] = "#438600"
+    gif.color_config["color_pixel_off_dark"] = "#346800"
+    gif.color_config["color_pixel_on_light"] = "#B9FF73"
+    gif.color_config["color_pixel_on_dark"] = "#6AD500"
+    assert compare_gif(gif, "readme_content/frog_jump.gif")
+
     gif = GIF(columns=22, rows=24, progress_bar=False)
     gif.add_image_fragment(
         image_path="readme_content/dino.png",
@@ -11,12 +24,3 @@ def test_readme_content():
         speed=24,
     )
     assert compare_gif(gif, "readme_content/dino.gif")
-
-    gif = GIF(columns=20, rows=20, progress_bar=False)
-    gif.add_image_fragment(
-        image_path="readme_content/frog_jump.png",
-        direction="left",
-        duration=100,
-        speed=21,
-    )
-    assert compare_gif(gif, "readme_content/frog_jump.gif")
